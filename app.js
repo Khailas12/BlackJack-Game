0289133f5c2e29f2e7dac6f7b867719a2e5f9196
Khailas R <khailas303@gmail.com>
2021-10-17 16:18:12 +0530

    new card added and error fixed

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ let hasBlackJack = false;
 let isAlive = true;
 
 let messageEl = document.getElementById('message-el');
+let sumEl = document.getElementById('sum-el');
+let cardsEl = document.getElementById('cards-el');
 let message = '';
 
 
 function startGame() {
+
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    sumEl.textContent = "Sum: " + sum;
+
     if (sum <= 21) {
-        console.log('Do you wanna draw a new card');
+        message = 'Do you wanna draw a new card?';
     } else if (sum === 21) {
         message = "You've got BlackJack";
         hasBlackJack = true;
@@ -20,8 +26,12 @@ function startGame() {
         message = 'You are outta the game';
         isAlive = false;
     }
-    messageEl.textContent = message
+    messageEl.textContent = message;
 }
 
-console.log(hasBlackJack);
-console.log(isAlive);  
+
+function newCard() {
+    let card = 7;
+    sum += card;
+    startGame()
+}
